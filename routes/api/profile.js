1d59acab509b79cc1f62e5dc33936095bdfcd6b1
keren0908 <akayayy@gmail.com>
2019-07-02 21:37:08 -0400

    add profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -109,6 +109,61 @@ router.post('/',
 
 });
 
+// @route    Put api/profile/experience
+// @desc     Add profile experience 
+// @access   Private
+router.put('/experience', 
+    [   
+        auth, 
+        [
+            check('title', 'Title is required').not().isEmpty(),
+            check('company', 'Company is required').not().isEmpty(),
+            check('from', 'From Date is required').not().isEmpty()
+        ]
+    ],
+     async (req, res) => {
+         const errors = validationResult(req);
+         if(!errors.isEmpty()){
+             return res.status(400).json({ errors: errors.array() });
+         }
+
+         const {
+             title,
+             company,
+             location,
+             from,
+             to,
+             current,
+             description
+         } = req.body;
+
+        // Build experience object
+       const newExp = {
+           title,
+           company,
+           location,
+           from,
+           to,
+           current,
+           description
+       }
+
+       try{
+            const profile = await Profile.findOne({user: req.user.id});
+            console.log(profile);
+            profile.experiences.unshift(newExp);
+
+            await profile.save();
+            res.json(profile);
+
+
+       }catch(err){
+           console.log(err.message);
+           res.status(500).send("Server Error");
+       }
+
+});
+
 // @route    GET api/profile
 // @desc     GET all profiles
 // @access   Public
